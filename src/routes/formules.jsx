c07e97961955ed82ce5e607800643dabb5d6cf41
nocolor <nocolor.web@gmail.com>
2025-03-03 01:38:33 +0300

    Броски кубов, починка стилей

--- src/routes/formules.jsx
+++ src/routes/formules.jsx
@@ -2,28 +2,29 @@ import React, { useState } from "react";
 import classes from "../styles/classes.module.scss";
 import NavMenu from "./components/menu";
 
-function DoWeakBlow(Power, Modificator, CubeResult) {
-  let result = CubeResult - 4 + Math.round(0.25 * Power) + Modificator
+function DoWeakBlow(Power, Modificator, CubeResult1) {
+  let result = CubeResult1 - 4 + Math.round(0.25 * Power) + Modificator
   return result >= 0 ? result : 0;
 
 }
-function DoStrongBlow(Power, Modificator, CubeResult) {
-  let result = CubeResult + Math.round(0.5 * Power) + Modificator;
+function DoStrongBlow(Power, Modificator, CubeResult1) {
+  let result = CubeResult1 + Math.round(0.5 * Power) + Modificator;
   return result >= 0 ? result : 0;
 }
-function DoKick(Power, Modificator, CubeResult) {
-  let result =  2 * CubeResult + Math.round(0.125 * Power) + Modificator;
+function DoKick(Power, Modificator, CubeResult1, CubeResult2) {
+  let result =  Math.floor((CubeResult1 + CubeResult2) / 2) + Math.round(0.125 * Power) + Modificator;
   return result >= 0 ? result : 0;
 }
-function DoPush(Modificator, CubeResult) {
-  let result = CubeResult - 2 + Modificator;
+function DoPush(Modificator, CubeResult1) {
+  let result = CubeResult1 - 2 + Modificator;
   return result >= 0 ? result : 0;
 }
 
 function Formules() {
   const [Power, setPower] = useState(0);
   const [Modificator, setModificator] = useState(0);
-  const [CubeResult, setCubeResult] = useState(0);
+  const [CubeResult1, setCubeResult1] = useState(0);
+  const [CubeResult2, setCubeResult2] = useState(0);
   const [TypeAttack, setTypeAttack] = useState("DoWeakBlow"); 
 
   const attackFunctions = {
@@ -36,7 +37,7 @@ function Formules() {
   function onClickButtonResult(typeAttack) {
     const attackFunction = attackFunctions[typeAttack];
     if (attackFunction) {
-      const result = attackFunction(Number(Power), Number(Modificator), Number(CubeResult));
+      const result = attackFunction(Number(Power), Number(Modificator), Number(CubeResult1), Number(CubeResult2));
       return <p>Ваш урон составил: {result}</p>;
     } else {
       console.error(`Функция ${typeAttack} не найдена.`);
@@ -50,12 +51,25 @@ function Formules() {
       <details>
         <summary className={classes.header}>Ближний бой</summary>
       <main className={classes.content}>
-        <input type="number" value={Power} name="PowerInput" onChange={event => setPower(event.target.value)} />
-        <label htmlFor="PowerInput">Ваша сила</label>
-        <input type="number" value={Modificator} name="ModificatorInput" onChange={event => setModificator(event.target.value)} />
-        <label htmlFor="ModificatorInput">Ваш модификатор</label>
-        <input type="number" value={CubeResult} name="CubeInput" onChange={event => setCubeResult(event.target.value)} />
-        <label htmlFor="CubeInput">Результат кубика</label>
+        <div>
+          <input type="number" value={Power} name="PowerInput" onChange={event => setPower(event.target.value)} />
+          <label htmlFor="PowerInput">Ваша сила</label>
+        </div>
+        <div>
+          <input type="number" value={Modificator} name="ModificatorInput" onChange={event => setModificator(event.target.value)} />
+          <label htmlFor="ModificatorInput">Ваш модификатор</label>
+        </div>
+        <div>
+          <input type="number" value={CubeResult1} name="CubeInput" onChange={event => setCubeResult1(event.target.value)} />
+          <label htmlFor="CubeInput">Результат кубиков</label>
+          {TypeAttack === "DoKick" ? 
+            <div>
+              <input type="number" value={CubeResult2} name="CubeInput2" onChange={event => setCubeResult2(event.target.value)} />
+              <label htmlFor="CubeInput2">Результат кубиков</label>
+            </div>  
+          : ""}
+          
+        </div>
         <select value={TypeAttack} onChange={event => setTypeAttack(event.target.value)}>
           <option value="DoWeakBlow">Слабый удар</option>
           <option value="DoStrongBlow">Сильный удар</option>
@@ -65,9 +79,17 @@ function Formules() {
         {onClickButtonResult(TypeAttack)} 
         <button className={classes.button} onClick={() => {
           setModificator(0)
-          setCubeResult(0)
+          setCubeResult1(0)
+          setCubeResult2(0)
           setPower(0)
         }}>Сбросить</button>
+        <button className={classes.button} onClick={() =>{
+          let number = Math.floor(Math.random() * 6) + 1;
+          let number2 = Math.floor(Math.random() * 6) + 1;
+          setCubeResult1(number);
+          setCubeResult2(number2);
+          console.log(number, number2)
+        }}>Бросить кубики</button>
       </main>
       </details>
 
